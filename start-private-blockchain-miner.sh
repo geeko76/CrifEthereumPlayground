f605c98230947cbcec3f9171b327f0af1f04219e
geeko76 <dotnetjunkie@gmail.com>
2016-06-14 12:00:46 +0200

    added rpcport

--- start-private-blockchain-miner.sh
+++ start-private-blockchain-miner.sh
@@ -1,1 +1,1 @@
-sudo geth --datadir $HOME/GethData --mine --minerthreads 2 --networkid 123321 --rpc --rpcapi "admin,eth,debug,miner,net,shh,txpool,personal,web3" --rpccorsdomain "*" --port 30309 --nat "any" console 
+sudo geth --datadir $HOME/GethData --mine --minerthreads 2 --networkid 123321 --rpc --rpcapi "admin,eth,debug,miner,net,shh,txpool,personal,web3" --rpccorsdomain "*" --rpcport "8545" --port 30309 --nat "any" console 
