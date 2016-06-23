0fc15b7e645112b1258bfe4d1bf748fe7941e188
geeko76 <dotnetjunkie@gmail.com>
2016-06-23 09:35:57 +0200

    Update configure-eth-netstats.sh

--- configure-eth-netstats.sh
+++ configure-eth-netstats.sh
@@ -1,16 +1,15 @@
 # !/bin/bash
-# bash configure-eth-netstats.sh <node_name> <ws_server> <ws_secret>
+# bash configure-eth-netstats.sh <port> <ws_secret>
 
 # sets up a eth-net-intelligence app.json for a local ethereum network cluster of nodes
-# - <node_name> the node name as will appear in the listing
-# - <ws_server> is the eth-netstats server
+# - <port> the node port for listing
 # - <ws_secret> is the eth-netstats secret
 
-node_name=$1
-ws_server=$2
-ws_secret=$3
+port=$1
+ws_secret=$2
 
 echo -e "["
-echo -e "  {\n    \"name\"        : \"$node_name\",\n    \"cwd\"         : \".\",\n    \"script\"      : \"app.js\",\n    \"log_date_format\"   : \"YYYY-MM-DD HH:mm Z\",\n    \"merge_logs\"    : false,\n    \"watch\"       : false,\n    \"max_restarts\"       : 10,\n    \"exec_interpreter\"  : \"node\",\n    \"exec_mode\"     : \"fork_mode\",\n    \"env\":\n    {\n      \"NODE_ENV\"    : \"production\",\n      \"RPC_HOST\"    : \"localhost\",\n      \"RPC_PORT\"    : \"8545\",\n      \"LISTENING_PORT\"    : \"30309\",\n      \"INSTANCE_NAME\"   : \"$node_name\",\n      \"WS_SERVER\"     : \"$ws_server\",\n      \"WS_SECRET\"     : \"$ws_secret\",\n      \"VERBOSITY\"     : 2,\n    }\n  }"
+echo -e "  {\n    \"name\"        : \"$node_name\",\n    \"cwd\"         : \".\",\n    \"script\"      : \"app.js\",\n    \"log_date_format\"   : \"YYYY-MM-DD HH:mm Z\",\n    \"merge_logs\"    : false,\n    \"watch\"       : false,\n    \"max_restarts\"       : 10,\n    \"exec_interpreter\"  : \"node\",\n    \"exec_mode\"     : \"fork_mode\",\n    \"env\":\n    {\n      \"PORT\"    : \"$port\",\n      \"WS_SECRET\"     : \"$ws_secret\",\n    }\n  }"
 echo "]"
+
 
