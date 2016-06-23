94dc85d580b212b779d09c3b67ff3de03b59dc73
geeko76 <dotnetjunkie@gmail.com>
2016-06-23 17:16:25 +0200

    Add files via upload

---
+++ start-private-blockchain-dev-monitor.sh
@@ -0,0 +1,4 @@
+cd $HOME/eth-netstats
+pm2 start gethcluster.json
+cd $HOME
+sudo geth --datadir $HOME/GethData --networkid "123321" --rpc --rpcapi "admin,eth,debug,miner,net,shh,txpool,personal,web3" --rpccorsdomain "*" --rpcport "8545"  --port "30309" --nat "any" console 
