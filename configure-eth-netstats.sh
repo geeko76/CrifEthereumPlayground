176436a03375bed02645e4c872157e61bf4bc2b0
geeko76 <dotnetjunkie@gmail.com>
2016-06-23 10:36:52 +0200

    Update configure-eth-netstats.sh

--- configure-eth-netstats.sh
+++ configure-eth-netstats.sh
@@ -11,5 +11,3 @@ ws_secret=$2
 echo -e "["
 echo -e "  {\n    \"name\"        : \"$node_name\",\n    \"cwd\"         : \".\",\n    \"script\"      : \"app.js\",\n    \"log_date_format\"   : \"YYYY-MM-DD HH:mm Z\",\n    \"merge_logs\"    : false,\n    \"watch\"       : false,\n    \"max_restarts\"       : 10,\n    \"exec_interpreter\"  : \"node\",\n    \"exec_mode\"     : \"fork_mode\",\n    \"env\":\n    {\n      \"PORT\"    : \"$port\",\n      \"WS_SECRET\"     : \"$ws_secret\",\n    }\n  }"
 echo "]"
-
-
