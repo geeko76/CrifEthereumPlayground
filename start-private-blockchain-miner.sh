1eed1b052dc141ad5d7c53b126943f64944d4fb2
geeko76 <dotnetjunkie@gmail.com>
2016-06-24 11:15:25 +0200

    Update start-private-blockchain-miner.sh

--- start-private-blockchain-miner.sh
+++ start-private-blockchain-miner.sh
@@ -1,1 +1,4 @@
+cd $HOME/eth-net-instelligence-api
+pm2 start gethcluster.json
+cd $HOME
 sudo geth --datadir $HOME/GethData --mine --minerthreads 2 --networkid 123321 --rpc --rpcapi "admin,eth,debug,miner,net,shh,txpool,personal,web3" --rpccorsdomain "*" --rpcport "8545" --port 30309 --nat "any" console 
